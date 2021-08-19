5f02c7e132505d36eea3b54a80eafec5f4103bfc
Oriakhi Collins <newowner@collins-macos.local>
2021-08-19 23:50:10 +0100

    Added Procfile ~~

--- public/functions.js
+++ public/functions.js
@@ -1,3 +1,17 @@
-<script>
-   
-</script>
+const email=document.getElementById('email');
+const message=document.getElementById('message');
+const form=document.getElementById('myForm');
+const errorElement=document.getElementById('error');
+form.addEventListener('submit',(e)=>{
+    let errors=[];
+    if(email.value===""|| email.value===null){
+        //
+        errors.push('Email is Required.');
+    }
+    if (errors.length>0) {
+        e.preventDefault();
+        errorElement.innerText=errors.join(', ')
+
+        
+    }
+})
